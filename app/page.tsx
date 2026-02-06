64400132e8936089c39410345a8747dd4ae2e4b5
orbz <orbzs085@gmail.com>
2026-02-06 12:46:38 +0900

    index page css (unfinished)

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="">
-        <h1 className="">React練習專案</h1>
-        <p className="">歡迎光臨我的介面，請點選下方按鈕進入記帳小工具</p>
+    <main className="stragecolor w-full h-full flex flex-col justify-center items-center pt-8">
+      <div className="w-3/5 h-1/3 flex flex-col justify-center items-end ">
+        <h1 className="text-6xl pb-4 pt-8 font-black">React練習專案</h1>
+        <p className="text-xl pb-8 font-semibold">請點選下方按鈕進入記帳頁面</p>
       </div>
-      <div className="">
-        <Link className="" href="/accounting">
+      <div className=" w-3/5 flex flex-col justify-center items-end">
+        <Link
+          className="text-lg font-bold bg-neutral-700 rounded-full p-6 px-12 text-[#d9ff41]
+"
+          href="/accounting"
+        >
           點此開始
         </Link>
       </div>
